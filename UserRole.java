b82903603384c80ee72e080bfedca367dfd91927
Shreyas Viswanathan <viswan27@purdue.edu>
2023-10-31 21:59:16 -0400

    Add enum type for user roles

---
+++ UserRole.java
@@ -0,0 +1,14 @@
+/**
+ * Project 4 - UserRole.java
+ * 
+ * For storing the available user roles in the application.
+ * 
+ * @author Shafer Anthony Hofmann, Qihang Gan, Shreyas Viswanathan, Nathan Pasic Miller, Oliver Long
+ * 
+ * @version October 31, 2023
+ */
+
+public enum UserRole {
+    CUSTOMER,
+    SELLER
+}
