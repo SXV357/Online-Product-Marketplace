12c81f09bd77e9beb26d6e5669c892ded0cbaa50
Shreyas Viswanathan <viswan27@purdue.edu>
2023-10-31 21:30:13 -0400

    Set up driver class for application

---
+++ Runner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Project 4 - Runner.java
+ * 
+ * Driver class for the application.
+ * 
+ * @author Shafer Anthony Hofmann, Qihang Gan, Shreyas Viswanathan, Nathan Pasic Miller, Oliver Long
+ * 
+ * @version October 31, 2023
+ */
+
+public class Runner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
