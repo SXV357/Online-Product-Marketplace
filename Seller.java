37ec69daef6056c79be463a2c0f6d1f943bc75ee
Oliver Long <longoliver2005@gmail.com>
2023-11-04 16:14:30 -0400

    Starting Seller Stuff

--- Seller.java
+++ Seller.java
@@ -7,6 +7,36 @@
  * 
  * @version November 2, 2023
  */
-public class Seller {
+public class Seller extends User{
+    private Store[] stores;
+
+    public void addStore(String storeName){
+        //Store newStore = new Store(storeName);
+
+        //database.add should add a new line to the appropriate database
+
+        String storeLine = String.join(",", this.UserID,storeName);
+        //database.add(STORE,storeLine);
+
+    }
+
+    public void modifyStore(String oldName, String newName){
+        //Store newStore = new Store(storeName);
+        //database.add(STORE, String oldName, String newName);
+    }
+
+    public Store[] getStores(){
+        //pull from stores database to see which stores are owned by this UserID
+        ArrayList<Stores>
+        String[] allStores;
+        //String[] allStores = database.get(STORES)
+        //assuming ownerID is stored in the first index
+        int idIndex = 0;
+        for(String line:allStores){
+            if
+        }
+    }
+
+
 
 }
