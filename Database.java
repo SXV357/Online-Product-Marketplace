5d3125fd4271eb3cbce0327242d641aa035adef4
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-11-05 17:28:46 -0500

    docs for database functionality + updates in user and runner based on updated db functionality

--- Database.java
+++ Database.java
@@ -28,6 +28,12 @@ public class Database {
         this.purchaseHistoryEntries = new ArrayList<>();
     }
 
+    /**
+     * Takes in the name of the file as input and returns a string containing all the headers associated with that file.
+     * 
+     * @param fileName The name of the file that the user wants to update
+     * @return The headers that are associated with that specific CSV file.
+     */
     public String getFileHeaders(String fileName) {
         String fileHeaders = "";
         switch (fileName) {
@@ -40,24 +46,45 @@ public class Database {
         return fileHeaders;
     }
 
-    public ArrayList<String> getDatabaseContents(String fileName) {
-        ArrayList<String> fileContents = new ArrayList<String>();
-        File output = new File(DATABASES_DIRECTORY + fileName);
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(output));
-            br.readLine(); // skipping the headers
-            String line;
-            while ((line = br.readLine()) != null) {
-                fileContents.add(line);
-                line = br.readLine();
+    /**
+     * Takes in an ID, then compares it with all entries in the users.csv database to check if there's a match
+     * 
+     * @param id The ID to compare with an existing entry in the users.csv database
+     * @return Returns whether the ID already exists in the users.csv database
+     */
+    public boolean checkUserIDMatch(int id) {
+        for (int i = 0; i < this.userEntries.size(); i++) {
+            String[] userRepresentation = this.userEntries.get(i).split(",");
+            if (id == Integer.parseInt(userRepresentation[0])) {
+                return true;
             }
-            br.close();
-        } catch (IOException e) {
-            return new ArrayList<String>();
         }
-        return fileContents;
+        return false;
     }
 
+    /**
+     * Takes in the user's email and password and retrieves a match for the credentials in the users.csv database.
+     * 
+     * @param email The user's email to compare with an existing entry in the users.csv database
+     * @param password The user's password to compare with an existing entry in the users.csv database
+     * @return A comma-separated string containing the matched user's information in the users.csv database
+     */
+    public String retrieveUser(String email, String password) {
+        for (int j = 0; j < this.userEntries.size(); j++) {
+            String[] userRepresentation = this.userEntries.get(j).split(",");
+            if (email.equals(userRepresentation[1]) && password.equals(userRepresentation[2])) {
+                return this.userEntries.get(j);
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Takes in a comma-separated string as an entry and appends it to the file specified by the file name
+     * 
+     * @param fileName The name of the file to append the entry to
+     * @param entry The entry to be appended to the file
+     */
     public void addToDatabase(String fileName, String entry) {
         switch (fileName) {
             case "users.csv": 
@@ -83,6 +110,12 @@ public class Database {
         }
     }
 
+    /**
+     * Takes in a comma-separated string as an entry and removes it from the file specified by the file name
+     * 
+     * @param fileName The name of the file to remove the entry from
+     * @param entry The entry to be removed from the file
+     */
     public void removeFromDatabase(String fileName, String entry) {
         switch (fileName) {
             case "users.csv": 
@@ -108,6 +141,13 @@ public class Database {
         }
     }
 
+    /**
+     * Takes in two comma separated strings, one representing the previous entry in the file specified and one representing the modified entry and updates that specified entry in the file with the modified entry
+     * 
+     * @param fileName The name of the file to be updated with the modified entry
+     * @param prevEntry The entry that already exists in the file
+     * @param newEntry The entry that will replace the previous entry in the specified file
+     */
     public void modifyDatabase(String fileName, String prevEntry, String newEntry) {
         switch (fileName) {
             case "users.csv": 
@@ -138,6 +178,36 @@ public class Database {
         }
     }
 
+    /**
+     * Takes in the name of the file as input and extracts all of its contents line-by-line
+     * 
+     * @param fileName The filename to extract the contents from
+     * @return An arraylist containing all the entries in the specified file
+     */
+    public ArrayList<String> getDatabaseContents(String fileName) {
+        ArrayList<String> fileContents = new ArrayList<String>();
+        File output = new File(DATABASES_DIRECTORY + fileName);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(output));
+            br.readLine(); // skipping the headers
+            String line;
+            while ((line = br.readLine()) != null) {
+                fileContents.add(line);
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException e) {
+            return new ArrayList<String>();
+        }
+        return fileContents;
+    }
+
+    /**
+     * Takes in the name of the file and overwrites it completely with the contents specified by the contents in the arraylist
+     * 
+     * @param fileName The name of the file whose contents need to be updated
+     * @param contents An arraylist representing the modified contents to be written to the file
+     */
     public void updateDatabaseContents(String fileName, ArrayList<String> contents) {
         File dir = new File(DATABASES_DIRECTORY);
         try {
--- User.java
+++ User.java
@@ -1,4 +1,3 @@
-
 /**
  * Project 4 - User.java
  * 
@@ -10,26 +9,11 @@
  */
 public class User {
     //Fields
-    private String email;  //Each user has a unique email
-    private String password;  // Password
-    private UserRole role;  //Each user has a role (Seller or Customer);
+    private static Database db = new Database();
+    private String email;
+    private String password;
+    private UserRole role; // User's role in the application (Seller or Customer);
     private int userID;  //Each user has a unique ID
-
-    //Constructor for when a user creates their account (userID must be created)
-    public User(String email, String password, UserRole role) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.userID = createID();
-    }
-
-    //Constructor for when you create a user with a KNOWN userID
-    public User(int userID, String email, String password, UserRole role) {
-        this.userID = userID;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
 
     //Blank Constructor for currentUser in runner
     public User() {
@@ -37,6 +21,22 @@ public class User {
         this.email = "";
         this.password = "";
         this.role = null;
+    }
+
+    //Constructor to initialize a new user when they create an account
+    public User(String email, String password, UserRole role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.userID = createID();
+    }
+
+    //Constructor to re-initialize a user when they log back in with the correct credentials
+    public User(int userID, String email, String password, UserRole role) {
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
 
     //Getters
@@ -56,7 +56,7 @@ public class User {
         return this.role;
     }
 
-    //Setters
+    //Setters (when the user wants to modify their account)
     public void setEmail(String email) {
         this.email = email;
     }
@@ -65,36 +65,17 @@ public class User {
         this.password = password;
     }
 
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-
     public int createID() {
-        int newID = 100000;  //This would be the first user's ID
-
-        //TODO implement a database method to check for existing userIDs
-/*        while (*//* Check Database users to see if someone already has this ID*//*) {
-            newID++;
-        }*/
-        return newID;
+        int currentID = 100000;  //This would be the first user's ID
+        while (db.checkUserIDMatch(currentID)) {
+            currentID++;
+        }
+        return currentID;
     }
 
     // this is what's being received by the addUsersToDatabase method in the database class
     @Override
     public String toString() {
         return String.format("%d,%s,%s,%s", this.userID, this.email, this.password, this.role);
-    }
-
-    //This compares the current user's email and password to another user's passed in as a parameter
-    //Used to log user in
-    @Override
-    public boolean equals(Object o){
-        if (o instanceof User) {
-            User user = (User) o;
-            if (this.email.equals(user.getEmail()) && this.password.equals(user.getPassword())) {
-                return true;
-            }
-        }
-        return false;
     }
 }
