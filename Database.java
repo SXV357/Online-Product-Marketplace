ffa33ef3ae03c099447e08fae540b5ae7109849f
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-11-06 00:45:04 -0500

    add functionality to search specific columns in db and check entry existence

--- Database.java
+++ Database.java
@@ -5,7 +5,15 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
+/**
+ * Project 4 - Database.java
+ * 
+ * Class that handles all database access and modification functionality related to the application.
+ * 
+ * @author Shafer Anthony Hofmann, Qihang Gan, Shreyas Viswanathan, Nathan Pasic Miller, Oliver Long
+ * 
+ * @version November 5, 2023
+ */
 public class Database {
     private static final String DATABASES_DIRECTORY = "databases/";
     private static final String usersDatabaseHeaders = "ID,Email,Password,Role";
@@ -19,7 +27,8 @@ public class Database {
     private ArrayList<String> productEntries;
     private ArrayList<String> shoppingCartEntries;
     private ArrayList<String> purchaseHistoryEntries;
-
+    
+    // Initializing all local arraylists for usage when adding, updating, or removing entries from each of the databases
     public Database() {
         this.userEntries = new ArrayList<>();
         this.storeEntries = new ArrayList<>();
@@ -47,7 +56,19 @@ public class Database {
     }
 
     /**
-     * Takes in an ID, then compares it with all entries in the users.csv database to check if there's a match
+     * Takes in the name of the file and a column index and returns whether that index is within bounds of how many ever columns exist in the given file
+     * 
+     * @param fileName The file using which the validity of the index will be determined
+     * @param index The index of the specified column
+     * @return If the column index is within bounds of the total number of columns in the file
+     */
+    public boolean checkColumnBounds(String fileName, int index) {
+        String[] columns = getFileHeaders(fileName).split(",");
+        return ((index >= 0) && (index <= columns.length - 1));
+    }
+
+    /**
+     * Takes in an ID, then compares it with all entries in the users.csv database to check if there's a match. Used for assigning an ID to a new user.
      * 
      * @param id The ID to compare with an existing entry in the users.csv database
      * @return Returns whether the ID already exists in the users.csv database
@@ -148,34 +169,102 @@ public class Database {
      * @param prevEntry The entry that already exists in the file
      * @param newEntry The entry that will replace the previous entry in the specified file
      */
-    public void modifyDatabase(String fileName, String prevEntry, String newEntry) {
+    public boolean modifyDatabase(String fileName, String prevEntry, String newEntry) {
+        boolean databaseModified = false;
         switch (fileName) {
             case "users.csv": 
                             int prevUserIdx = this.userEntries.indexOf(prevEntry);
-                            this.userEntries.set(prevUserIdx, newEntry);
-                            updateDatabaseContents(fileName, this.userEntries);
+                            if (prevUserIdx != -1) {
+                                this.userEntries.set(prevUserIdx, newEntry);
+                                updateDatabaseContents(fileName, this.userEntries);
+                                databaseModified = true;
+                            }
                             break;
             case "stores.csv":
                             int prevStoreIdx = this.storeEntries.indexOf(prevEntry);
-                            this.storeEntries.set(prevStoreIdx, newEntry);;
-                            updateDatabaseContents(fileName, storeEntries);
+                            if (prevStoreIdx != -1) {
+                                this.storeEntries.set(prevStoreIdx, newEntry);
+                                updateDatabaseContents(fileName, storeEntries);
+                                databaseModified = true;
+                            }
                             break;
             case "products.csv":
                             int prevProductIdx = this.productEntries.indexOf(prevEntry);
-                            this.productEntries.set(prevProductIdx, newEntry);
-                            updateDatabaseContents(fileName, productEntries);
+                            if (prevProductIdx != -1) {
+                                this.productEntries.set(prevProductIdx, newEntry);
+                                updateDatabaseContents(fileName, productEntries);
+                                databaseModified = true;
+                            }
                             break;
             case "shoppingCarts.csv":
                             int prevShoppingCartIdx = this.shoppingCartEntries.indexOf(prevEntry);
-                            this.shoppingCartEntries.set(prevShoppingCartIdx, newEntry);
-                            updateDatabaseContents(fileName, shoppingCartEntries);
+                            if (prevShoppingCartIdx != -1) {
+                                this.shoppingCartEntries.set(prevShoppingCartIdx, newEntry);
+                                updateDatabaseContents(fileName, shoppingCartEntries);
+                                databaseModified = true;
+                            }
                             break;
             case "purchaseHistories.csv":
                             int prevPurchaseHistoryIdx = this.purchaseHistoryEntries.indexOf(prevEntry);
-                            this.purchaseHistoryEntries.set(prevPurchaseHistoryIdx, newEntry);
-                            updateDatabaseContents(fileName, purchaseHistoryEntries);
+                            if (prevPurchaseHistoryIdx != -1) {
+                                this.purchaseHistoryEntries.set(prevPurchaseHistoryIdx, newEntry);
+                                updateDatabaseContents(fileName, purchaseHistoryEntries);
+                                databaseModified = true;
+                            }
                             break;
         }
+        return databaseModified;
+    }
+
+    /**
+     * Searches for the entry parameter in the file specified by filename and returns whether it exists or not
+     * 
+     * @param fileName The name of the file to within which to look for
+     * @param entry A comma-separated string representing a possible entry in the file
+     * @return The existence of the entry in the specified file
+     */
+    public boolean checkEntryExists(String fileName, String entry) {
+        File target = new File(DATABASES_DIRECTORY + fileName);
+        try (BufferedReader br = new BufferedReader(new FileReader(target))) {
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.equals(entry)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Searches the column specified by the index in the specified file and returns all rows where the value in that column matches the value of the search parameter
+     * 
+     * @param fileName The file to search for
+     * @param index The index of the column to search for
+     * @param searchParam The parameter to compare a value in the specified column to
+     * @return An arraylist of the matched rows
+     */
+    public ArrayList<String> getMatchedEntries(String fileName, int index, String searchParam) {
+        ArrayList<String> matchedEntries = new ArrayList<>();
+        if (checkColumnBounds(fileName, index)) {
+            File target = new File(DATABASES_DIRECTORY + fileName);
+            try (BufferedReader br = new BufferedReader(new FileReader(target))) {
+                br.readLine(); // skip the header
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] contents = line.split(",");
+                    if (contents[index].equals(searchParam)) {
+                        matchedEntries.add(line);
+                    }
+                } 
+            } catch (IOException e) {
+                return new ArrayList<String>();
+            }
+        }
+        return matchedEntries;
     }
 
     /**
--- TestDatabase.java
+++ TestDatabase.java
@@ -1,13 +1,80 @@
 public class TestDatabase {
     public static void main(String[] args) {
         Database db = new Database();
-        testAddNewEntryToDatabase(db);
+        testGetFileHeaders(db);
+        testAddInitialDatabaseEntries(db);
+        testAddAdditionalDatabaseEntries(db);
+        testSuccessfulDatabaseModification(db);
+        testUnsuccessfulDatabaseModification(db);
     }
 
-    static void testAddNewEntryToDatabase(Database db) {
-        String newUser = "100000,something1@yahoo.com,665GVYHF,Customer";
-        db.addToDatabase("users.csv", newUser);
-        String newUserTwo = "100001,something2@yahoo.com,45354HVTFHT,Seller";
-        db.addToDatabase("users.csv", newUserTwo);
+    static void testGetFileHeaders(Database db) {
+        String[] fileNames = {"stores.csv", "purchaseHistories.csv", "users.csv", "shoppingCarts.csv", "products.csv"};
+        for (int i = 0; i < fileNames.length; i++) {
+            System.out.printf("File headers associated with %s: %s%n", fileNames[i], db.getFileHeaders(fileNames[i]));
+        }
+    }
+
+    static void testAddInitialDatabaseEntries(Database db) {
+        // initially adding one entry to each of the databases
+        String userEntry = "100000,blabla@yahoo.com,abc123,Customer";
+        String storeEntry = "100001,Store1,5";
+        String productEntry = "100001,Store1,Burrito,50,5.99,This is a breakfast burrito";
+        String shoppingCartEntry = "100000,100001,Store1,Burrito,10,59.9";
+        String purchaseHistoryEntry = "100000,100001,Store1,Burrito,10,59.9";
+        db.addToDatabase("users.csv", userEntry);
+        db.addToDatabase("stores.csv", storeEntry);
+        db.addToDatabase("products.csv", productEntry);
+        db.addToDatabase("shoppingCarts.csv", shoppingCartEntry);
+        db.addToDatabase("purchaseHistories.csv", purchaseHistoryEntry);
+    }
+
+    static void testAddAdditionalDatabaseEntries(Database db) {
+        // adding an additional entry to each of the databases
+        String newUserEntry = "100001,blabla@gmail.com,abc123,Customer";
+        String newStoreEntry = "100002,Store2,10";
+        String newProductEntry = "100002,Store2,Apple,10,2,This is an apple";
+        String newShoppingCartEntry = "100001,100002,Store2,Apple,5,10";
+        String newPurchaseHistoryEntry = "100001,100002,Store2,Apple,5,10";
+        db.addToDatabase("users.csv", newUserEntry);
+        db.addToDatabase("stores.csv", newStoreEntry);
+        db.addToDatabase("products.csv", newProductEntry);
+        db.addToDatabase("shoppingCarts.csv", newShoppingCartEntry);
+        db.addToDatabase("purchaseHistories.csv", newPurchaseHistoryEntry);
+    }
+
+    static void testSuccessfulDatabaseModification(Database db) {
+        // modifying entries that already exist within the database
+        String currUserEntry = "100001,blabla@gmail.com,abc123,Customer";
+        String currStoreEntry = "100002,Store2,10";
+        String currProductEntry = "100002,Store2,Apple,10,2,This is an apple";
+        String currShoppingCartEntry = "100001,100002,Store2,Apple,5,10";
+        String currPurchaseHistoryEntry = "100001,100002,Store2,Apple,5,10";
+        // if user for example chooses to modify their email
+        boolean userModified = db.modifyDatabase("users.csv", currUserEntry, "100001,hello123@yahoo.com,abc123,Customer");
+        // if for example, a seller chooses to add more products to one of their existing stores
+        boolean storeModified = db.modifyDatabase("stores.csv", currStoreEntry, "100002,Store2,20");
+        // if for example, a seller chooses to modify certain details associated with one of the products in their store
+        boolean productModified = db.modifyDatabase("products.csv", currProductEntry, "100002,Store2,Apple,20,5,This is an apple");
+        // if the customer wants to update the quantity of the item in their existing shopping cart
+        boolean shoppingCartModified = db.modifyDatabase("shoppingCarts.csv", currShoppingCartEntry, "100001,100002,Store2,Apple,10,50");
+        boolean purchaseHistoryModified = db.modifyDatabase("purchaseHistories.csv", currPurchaseHistoryEntry, "100001,100002,Store2,Apple,10,50");
+        assert userModified && storeModified && productModified && shoppingCartModified && purchaseHistoryModified;
+    }
+
+    static void testUnsuccessfulDatabaseModification(Database db) {
+        // modifying entries that don't exist in the database
+        String nonExistentUserEntry = "100678,hello@yahoo.com,76ybgjh,Seller";
+        String nonExistentStoreEntry = "100912,Store100,90";
+        String nonExistentProductEntry = "100912,Store100,Apple,10,2,This is an apple";
+        String nonExistentShoppingCartEntry = "100678,100912,Store100,Apple,5,10";
+        String nonExistentPurchaseHistoryEntry = "100678,100912,Store100,Apple,5,10";
+        // no changes should be reflected in any of the databases since none of the entries exist
+        boolean userModified = db.modifyDatabase("users.csv", nonExistentUserEntry, "100678,hello@yahoo.com,jhghgf,Seller");
+        boolean storeModified = db.modifyDatabase("stores.csv", nonExistentStoreEntry, "100912,Store102,90");
+        boolean productModified = db.modifyDatabase("products.csv", nonExistentProductEntry, "100912,Store100,Apple,10,3,This is an apple");
+        boolean shoppingCartModified = db.modifyDatabase("shoppingCarts.csv", nonExistentShoppingCartEntry, "100678,100912,Store100,Apple,5,15");
+        boolean purchaseHistoryModified = db.modifyDatabase("purchaseHistories.csv", nonExistentPurchaseHistoryEntry, "100678,100912,Store100,Apple,5,15");
+        assert !userModified && !storeModified && !productModified && !shoppingCartModified && !purchaseHistoryModified;
     }
 }
