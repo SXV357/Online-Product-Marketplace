a6aa3688f4dc01257f9e7e0abc5623a26451e9f8
Oliver Long <longoliver2005@gmail.com>
2023-11-19 23:18:52 -0500

    Started templating

--- SellerClient.java
+++ SellerClient.java
@@ -1,3 +1,36 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class SellerClient {
     
+    private Socket socket;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+
+    //instance variable for convenience
+    //saves repeated server requests by always storing this data
+    private Seller seller;
+    public SellerClient(Socket socket, Seller seller) throws IOException{
+        this.socket = socket;
+        this.oos = new ObjectOutputStream(socket.getOutputStream());
+        this.ois = new ObjectInputStream(socket.getInputStream());
+
+        this.seller = seller;
+    }
+
+    public void signOut() throws IOException{
+        InitialClient initialClient = new InitialClient(socket);
+        initialClient.start();
+    }
+
+    //TODO
+    public void homepage(){
+        //call seller GUI
+        //TODO sellerGUI constructor needs more parameters
+        SellerGUI sellerGUI = new SellerGUI(seller.getEmail());
+        //sellerGUI main is the seller homepage
+        sellerGUI.main(null);
+    }
 }
