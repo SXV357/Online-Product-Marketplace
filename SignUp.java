d552dbf2eeecd97dac6549b9d7f09a31f5c2a8ba
QihangGan <1819719438@qq.com>
2023-11-20 00:35:52 -0500

    this clas has test Seller Store and Product class

--- SignUp.java
+++ SignUp.java
@@ -11,91 +11,75 @@ import java.awt.event.ActionListener;
  * @author Shafer Anthony Hofmann, Qihang Gan, Shreyas Viswanathan, Nathan Pasic
  *         Miller, Oliver Long
  *
- * @version November 18, 2023
+ * @version November 19, 2023
  */
-
-public class SignUpForm extends JFrame implements ActionListener {
+public class SignUp extends JFrame {
     private JTextField emailField;
     private JPasswordField passwordField;
     private JPasswordField confirmField;
+    private JComboBox<UserRole> roleComboBox;
     private JButton confirmButton;
+    private Database db;
 
-    /**
-     * Constructor to initialize the Signup Form UI.
-     */
-    public SignUpForm() {
-        // Set window title
-        super("Sign Up");
+    public SignUp(Database database) {
+        this.db = database;
+        createUI();
+    }
 
-        // Initialize text fields and button
-        emailField = new JTextField(20);
-        passwordField = new JPasswordField(20);
-        confirmField = new JPasswordField(20);
-        confirmButton = new JButton("Confirm");
+    private void createUI() {
+        setTitle("Sign Up");
+        setSize(300, 250);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // Set layout
-        setLayout(new FlowLayout());
+        setLayout(new GridLayout(5, 2, 5, 5));
+
         add(new JLabel("Email:"));
+        emailField = new JTextField();
         add(emailField);
+
         add(new JLabel("Password:"));
+        passwordField = new JPasswordField();
         add(passwordField);
+
         add(new JLabel("Confirm Password:"));
+        confirmField = new JPasswordField();
         add(confirmField);
+
+        add(new JLabel("Role:"));
+        roleComboBox = new JComboBox<>(UserRole.values());
+        add(roleComboBox);
+
+        confirmButton = new JButton("Confirm");
+        confirmButton.addActionListener(this::confirmAction);
         add(confirmButton);
-
-        // Add action listener to the button
-        confirmButton.addActionListener(this);
-
-        // Set window size and behavior
-        setSize(300, 150);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
     }
 
-    /**
-     * Action performed when the Confirm button is clicked.
-     *
-     * @param e The ActionEvent triggering this method.
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == confirmButton) {
-            String email = emailField.getText();
-            char[] password = passwordField.getPassword();
-            char[] confirm = confirmField.getPassword();
+    private void confirmAction(ActionEvent e) {
+        String email = emailField.getText();
+        String password = new String(passwordField.getPassword());
+        String confirmPassword = new String(confirmField.getPassword());
 
-            // Actual registration logic should be added here
-            if (isPasswordMatching(password, confirm)) {
-                JOptionPane.showMessageDialog(this, "Registration Successful!");
-            } else {
-                JOptionPane.showMessageDialog(this, "Passwords do not match!");
-            }
+        if (!password.equals(confirmPassword)) {
+            JOptionPane.showMessageDialog(this, "Passwords do not match!");
+            return;
         }
+
+        if (db.retrieveUserMatchForSignUp(email) != null) {
+            JOptionPane.showMessageDialog(this, "Email already in use. Please use a different email.");
+            return;
+        }
+
+        UserRole role = (UserRole) roleComboBox.getSelectedItem();
+        User newUser = new User(email, password, role);
+        db.addToDatabase("users.csv", newUser.toString());
+
+        JOptionPane.showMessageDialog(this, "Registration successful!");
+        this.dispose();
     }
 
-    /**
-     * Checks if the entered password and confirmation match.
-     *
-     * @param pass        The password entered by the user.
-     * @param confirmPass The confirmation password entered by the user.
-     * @return True if the passwords match, False otherwise.
-     */
-    private boolean isPasswordMatching(char[] pass, char[] confirmPass) {
-        return Arrays.equals(pass, confirmPass);
-    }
-
-    /**
-     * The main method to run the GUI, ensuring it operates within the event dispatch thread.
-     *
-     * @param args Command-line arguments (not used).
-     */
     public static void main(String[] args) {
-        // GUI operations must run in the event dispatch thread
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new SignUpForm();
-            }
-        });
+        // Ensure the GUI is created in the Event Dispatch Thread for thread safety
+        SwingUtilities.invokeLater(() -> new SignUp(new Database()).setVisible(true));
     }
 }
